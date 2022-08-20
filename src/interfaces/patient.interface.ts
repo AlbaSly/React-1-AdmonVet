d40c29c74e0b8334616bd7632e80256bfeccee81
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-20 02:29:28 -0500

    Change interfaces file name from IPatient.ts to patient.interface.ts

---
+++ src/interfaces/patient.interface.ts
@@ -0,0 +1,18 @@
+export interface IPatient {
+    id?: number,
+    pet: string,
+    owner: string,
+    email: string,
+    date: Date,
+    description: string
+}
+
+export const InitialPatient: IPatient = {
+    pet: '',
+    owner: '',
+    email: '',
+    date: new Date(),
+    description: ''
+}
+
+export const InitialPatientArray: IPatient[] = [];
