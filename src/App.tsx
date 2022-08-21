2fbff68aee9a9b52b24a87c9ff62afc4b4170098
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-21 16:54:49 -0500

    Add stupid forgotten change to import useEffect in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, Dispatch, SetStateAction } from "react";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import PatientList from "./components/PatientList";
