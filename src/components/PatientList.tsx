d63e32ff0b8f83a5655aa8fcb7780cd4155d4177
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-21 17:00:50 -0500

    Update PatientList component adding functions, setting styles for the component and props destructuring

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,6 +1,41 @@
+import {useState, useEffect, Dispatch, SetStateAction } from "react";
+//Components
 import Patient from "./Patient";
+import Alert from "./Alert";
+//Interfaces (props, objects, etc)
+import { PropsPatientListComponent } from "../interfaces/components/components.interface";
+import { IPatient, InitialPatient } from "../interfaces/patient.interface";
+import { AlertDefaultValue, IAlert } from "../interfaces/alert.interface";
 
-const PatientList = (): JSX.Element => {
+const PatientList = (props: PropsPatientListComponent): JSX.Element => {
+    const [alertInfo, setAlertInfo]: [IAlert, Dispatch<SetStateAction<IAlert>>] = useState(AlertDefaultValue);
+    const [showAlert, setShowAlert]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false);
+
+    const {
+        patientState,
+        patientState: {setPatient},
+        patientListState: {patientList, setPatientList},
+        formEditionState
+    } = props;
+
+    function deletePatient(id: string): void {
+        if (id) {
+            const patientListUpdated: IPatient[] = [...patientList.filter((patient: IPatient) => patient.id !== id)];
+            
+            setPatientList(patientListUpdated);
+            setPatient(InitialPatient);
+        
+            setAlertInfo({type: 'alert', msg: 'Paciente eliminado correctamente'});
+            
+            setShowAlert(true);
+            setTimeout(() => {
+
+                setShowAlert(false);
+                setAlertInfo(AlertDefaultValue);
+            }, 1500);
+        }
+    }
+
     return (
         <section className="md:w-1/2 lg:w-3/5 my-10 md:my-0">
             <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
@@ -10,14 +45,24 @@ const PatientList = (): JSX.Element => {
                 <span className="text-purple-500 font-bold">Citas</span>
             </p>
 
+            {showAlert ? <Alert type={alertInfo.type} msg={alertInfo.msg} /> : ''}
+
             <div className="md:h-screen md:overflow-y-auto overflow-x-hidden">
-                <Patient></Patient>
-                <Patient></Patient>
-                <Patient></Patient>
-                <Patient></Patient>
-                <Patient></Patient>
-                <Patient></Patient>
-                <Patient></Patient>
+                {
+                    !patientList.length
+
+                    ? <p className="text-center font-bold text-yellow-600">Comienza añadiendo un paciente...</p> 
+                    
+                    : patientList.map((patient: IPatient) => 
+                        <Patient 
+                            key={patient.id} 
+                            patient={patient}
+                            deletePatient={deletePatient}
+                            patientState={patientState}
+                            formEditionState={formEditionState}
+                        />
+                    )
+                }
             </div>
         </section>
     );
