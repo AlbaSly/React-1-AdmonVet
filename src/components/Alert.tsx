3fa25ddb36fb21708e2763ff395ae86c3bd70a3e
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-20 22:15:31 -0500

    Set props deestructuring

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,9 +1,11 @@
 import { IAlert } from "../interfaces/alert.interface";
 
 const Alert = (props: IAlert): JSX.Element => {
+    const {msg}: IAlert = props;
+
     return (
         <div id="alert" className={`showing p-2 ${props.error ? 'bg-red-500 ' : 'bg-green-500'} fixed md:absolute w-full top-0 left-0 shadow-md font-bold text-white text-center`}>
-            <p>{props.msg}</p>
+            <p>{msg}</p>
         </div>
     );
 }
