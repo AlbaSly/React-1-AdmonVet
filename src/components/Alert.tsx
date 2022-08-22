a06e5c200a344fcd45f867d6258b8f79fd3b6be0
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-22 15:05:24 -0500

    Update conditional styles

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,10 +3,10 @@ import { IAlert } from "../interfaces/alert.interface";
 const Alert = (props: IAlert): JSX.Element => {
     const {type, msg}: IAlert = props;
 
-    const bgColor: string = type === 'success' ? 'bg-green-500' : type === 'error' ? 'bg-red-500' : 'bg-yellow-500';
+    const bgColor: string = type === 'success' ? 'bg-green-500 fixed md:absolute' : type === 'error' ? 'bg-red-500 fixed md:absolute' : 'bg-yellow-500 fixed';
 
     return (
-        <div id="alert" className={`showing p-2 ${bgColor} fixed md:absolute w-full top-0 left-0 shadow-md font-bold text-white text-center`}>
+        <div id="alert" className={`showing p-2 ${bgColor} w-full top-0 left-0 z-20 shadow-md font-bold text-white text-center`}>
             <p>{msg}</p>
         </div>
     );
